a330f0a7a5ab13f532fdca05a07cb29bd95afe47
wolede <wolede.adeniyi@gmail.com>
2020-06-03 23:21:08 +0100

    remove serverside injected css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,16 +5,25 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { shuldrzTheme } from 'styles/theme'
 import { useStyles } from 'styles/global'
 
-  const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }) => {
     const classes = useStyles();
-      return (
-          <>
-			<ThemeProvider theme={shuldrzTheme}>
-				<CssBaseline />
-				<Component {...pageProps} className={classes.html}/>
-			</ThemeProvider>
-          </>
-      )
-  }
+
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <>
+        <ThemeProvider theme={shuldrzTheme}>
+            <CssBaseline />
+            <Component {...pageProps} className={classes.html}/>
+        </ThemeProvider>
+        </>
+    )
+}
   
   export default MyApp
