87d91e802b7b051d32bd643ab371b58935066772
wolede <wolede.adeniyi@gmail.com>
2020-06-22 02:56:45 +0100

    made login the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,24 @@
 import Head from 'next/head'
 import axios from "axios"
 import Link from 'next/link'
+import Login from './login'
 
 const Home = ({ users }) => {
   return (
-    <div>
-      <Head>
-        <title>Shuldrz | Here for a lean-on</title>
-      </Head>
-      {users.map(user => (
-        <div key={user.id}>
-          {user.username}
-        </div>
-      ))}
-      <Link href="/login"><a>Login</a></Link>
-      <Link href="/signup"><a>Signup</a></Link>
-      <Link href="/app"><a>user home</a></Link>
-    </div>
+    <Login/>
+    // <div>
+    //   <Head>
+    //     <title>Shuldrz | Here for a lean-on</title>
+    //   </Head>
+    //   {users.map(user => (
+    //     <div key={user.id}>
+    //       {user.username}
+    //     </div>
+    //   ))}
+    //   <Link href="/login"><a>Login</a></Link>
+    //   <Link href="/signup"><a>Signup</a></Link>
+    //   <Link href="/app"><a>user home</a></Link>
+    // </div>
   )
 }
 
