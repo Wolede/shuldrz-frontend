c71d7aa61d3ae0bd0c9864c35e5498c81f8ef013
wolede <wolede.adeniyi@gmail.com>
2020-08-29 05:07:29 +0100

    centered profileCard

--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -31,17 +31,19 @@ const ProfileCard = (props) => {
 
     return (
         <Paper padding="1.5rem 1.5rem 1.5rem 1.5rem" marginBottom="2rem">
-            <Box display="flex" flexDirection="column" alignItems='flex-start' justifyContent="center">
-                <Avatar
-                    alt={username}
-                    src={profileImage ? profileImage.url : '/empty'}
-                    size="medium"
-                    autoWidth
-                    marginBottom="1.5rem"
-                />
+            <Box display="flex" flexDirection="column" alignItems='center' justifyContent="center">
+                <Box display="flex" justifyContent="center" width="100%">
+                    <Avatar
+                        alt={username}
+                        src={profileImage ? profileImage.url : '/empty'}
+                        size="small"
+                        // autoWidth
+                        marginBottom="1.5rem"
+                    />
+                </Box>
                 <Typography variant="h4" className={classes.headerText}>{username}</Typography>
 
-                <Typography variant="body1" className={classes.text}>{experience ? experience : '- - -'}</Typography>
+                <Typography variant="body1" align="center" className={classes.text}>{experience ? experience : '- - -'}</Typography>
 
                 <Box display="flex" justifyContent="center" flexWrap="wrap">
                     {userType === 'Volunteer' &&
@@ -50,7 +52,7 @@ const ProfileCard = (props) => {
                     <Chip label={heart ? heart.count.toString() : '0'} heart color="paper"/>
                 </Box>
 
-                <Box width="100%" display="flex" justifyContent="flex-start" flexWrap="wrap" className={classes.buttonGroup}>
+                <Box width="100%" display="flex" justifyContent="center" flexWrap="wrap" className={classes.buttonGroup}>
                     {user.username !== username && (
                     <div>
                         <Button variant="contained" size="small" color="primary" onClick={handleMessageUser}>message</Button>
