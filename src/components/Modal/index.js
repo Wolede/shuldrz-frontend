f277d204c0a38430c12a4e76b5b96795866e1ca1
Fisayo Watti <wf.watti@gmail.com>
2021-02-15 11:57:02 +0100

    Create and use ViewLikes component to display users that liked a note
    Create and use ViewLikesModal to dispay detailed info on users that liked a note

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -14,146 +14,179 @@ import WallNote from '../WallNote'
 import NoteComments from '../NoteComments'
 import Share from '../Share'
 import { useStyles } from './style'
+import ViewLikesModal from 'components/ViewLikes/ViewLikesModal'
 
 const Modal = props => {
-	const classes = useStyles()
-	const { view, embedUrl, openModal, handleClose, disableBackdropClick, callback, formProps, viewNote, pageLimit, triggerUrl, callbacks, people, chat } = props
+  const classes = useStyles()
+  const {
+    view,
+    embedUrl,
+    openModal,
+    handleClose,
+    disableBackdropClick,
+    callback,
+    formProps,
+    viewNote,
+    pageLimit,
+    triggerUrl,
+    callbacks,
+    people,
+    chat,
+  } = props
 
-	console.log('PEOPLE', people)
+  console.log('PEOPLE', people)
 
-	return (
-		<MuiModal
-			aria-labelledby="simple-modal-title"
-			aria-describedby="simple-modal-description"
-			open={openModal}
-			onClose={handleClose}
-			disableBackdropClick={disableBackdropClick}
-			className={classes.root}
-		>
-			<Container maxWidth={view === 'video' ? 'md' : 'sm'} style={{ outline: 0 }} >
-				<Box marginTop="8rem">
-					{view === "video" &&
-						(
-							<div className={classes.embedContainer}>
-								<iframe width="560" height="315" src={embedUrl} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="modalFrame"></iframe>
-							</div>
-						)
-					}
+  return (
+    <MuiModal
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+      open={openModal}
+      onClose={handleClose}
+      disableBackdropClick={disableBackdropClick}
+      className={classes.root}
+    >
+      <Container
+        maxWidth={view === 'video' ? 'md' : 'sm'}
+        style={{ outline: 0 }}
+      >
+        <Box marginTop="8rem">
+          {view === 'video' && (
+            <div className={classes.embedContainer}>
+              <iframe
+                width="560"
+                height="315"
+                src={embedUrl}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="modalFrame"
+              ></iframe>
+            </div>
+          )}
 
-					{view === "writeJournal" &&
-						(
-							<Paper padding="1.5rem">
-								<AddJournalForm onClose={handleClose} pageLimit={pageLimit} prevJournal={props.prevJournal} />
-							</Paper>
-						)
-					}
-					{
-						view === 'review' && (
-							<Paper>
-								<ReviewForm chatProfile={props.chatProfile} prevReview={props.prevReview} />
-							</Paper>
-						)
-					}
-					{view === "forgotPassword" &&
-						(
-							<Paper padding="1.5rem">
-								<ForgotPasswordForm onClose={handleClose} />
-							</Paper>
-						)
-					}
+          {view === 'writeJournal' && (
+            <Paper padding="1.5rem">
+              <AddJournalForm
+                onClose={handleClose}
+                pageLimit={pageLimit}
+                prevJournal={props.prevJournal}
+              />
+            </Paper>
+          )}
+          {view === 'review' && (
+            <Paper>
+              <ReviewForm
+                chatProfile={props.chatProfile}
+                prevReview={props.prevReview}
+              />
+            </Paper>
+          )}
+          {view === 'forgotPassword' && (
+            <Paper padding="1.5rem">
+              <ForgotPasswordForm onClose={handleClose} />
+            </Paper>
+          )}
 
-					{view === "addInterestedTopics" &&
-						(
-							<Paper padding="1.5rem">
-								<AddTopicsForm onClose={handleClose} getSuggestedBuddies={callback} />
-							</Paper>
-						)
-					}
+          {view === 'addInterestedTopics' && (
+            <Paper padding="1.5rem">
+              <AddTopicsForm
+                onClose={handleClose}
+                getSuggestedBuddies={callback}
+              />
+            </Paper>
+          )}
 
-					{view === "addSessions" &&
-						(
-							<Paper padding="1.5rem">
-								<AddSessionsForm onClose={handleClose} submitNewChat={callbacks.submitNewChat} updateSelectedChat={callbacks.updateSelectedChat} />
-							</Paper>
-						)
-					}
+          {view === 'addSessions' && (
+            <Paper padding="1.5rem">
+              <AddSessionsForm
+                onClose={handleClose}
+                submitNewChat={callbacks.submitNewChat}
+                updateSelectedChat={callbacks.updateSelectedChat}
+              />
+            </Paper>
+          )}
 
+          {view === 'updateSession' && (
+            <Paper padding="1.5rem">
+              <UpdateSessionsForm
+                onClose={handleClose}
+                people={people}
+                chat={chat}
+              />
+            </Paper>
+          )}
 
-					{view === "updateSession" &&
-						(
-							<Paper padding="1.5rem">
-								<UpdateSessionsForm onClose={handleClose} people={people} chat={chat} />
-							</Paper>
-						)
-					}
-
-					{view === "schedule" &&
-						(
-							<Paper padding="1.5rem">
-								<ScheduleForm onClose={handleClose} formProps={formProps} />
-							</Paper>
-						)
-					}
-					{view === "share" &&
-						(
-							<Paper padding="1.5rem">
-								<Share note={viewNote} />
-							</Paper>
-						)
-					}
-					{view === "writeNote" &&
-						(
-							<Paper padding="1.5rem">
-								<AddNoteForm onClose={handleClose} triggerUrl={triggerUrl} prevNote={props.prevNote} />
-							</Paper>
-						)
-					}
-					{view === "viewNote" &&
-						(
-							<div>
-								<WallNote
-									modalIsOpen={true}
-									id={viewNote.id}
-									title={viewNote.title}
-									note={viewNote.note}
-									hearts={viewNote.hearts}
-									color={viewNote.color}
-									link={viewNote.link}
-									usersLiked={viewNote.usersLiked}
-									noteComments={viewNote.noteComments}
-									date={viewNote.date}
-									dedication={viewNote.dedication}
-									userData={viewNote.userData}
-									urlQuery={false}
-									isPublic={viewNote.isPublic}
-									triggerUrl={viewNote.triggerUrl}
-								/>
-							</div>
-						)
-					}
-					{view === "noteComments" &&
-						(
-							<NoteComments note={props.note} comments={props.comments} triggerUrl={props.triggerUrl} />
-						)
-					}
-				</Box>
-			</Container>
-		</MuiModal>
-	)
+          {view === 'schedule' && (
+            <Paper padding="1.5rem">
+              <ScheduleForm onClose={handleClose} formProps={formProps} />
+            </Paper>
+          )}
+          {view === 'share' && (
+            <Paper padding="1.5rem">
+              <Share note={viewNote} />
+            </Paper>
+          )}
+          {view === 'writeNote' && (
+            <Paper padding="1.5rem">
+              <AddNoteForm
+                onClose={handleClose}
+                triggerUrl={triggerUrl}
+                prevNote={props.prevNote}
+              />
+            </Paper>
+          )}
+          {view === 'viewNote' && (
+            <div>
+              <WallNote
+                modalIsOpen={true}
+                id={viewNote.id}
+                title={viewNote.title}
+                note={viewNote.note}
+                hearts={viewNote.hearts}
+                color={viewNote.color}
+                link={viewNote.link}
+                usersLiked={viewNote.usersLiked}
+                noteComments={viewNote.noteComments}
+                date={viewNote.date}
+                dedication={viewNote.dedication}
+                userData={viewNote.userData}
+                urlQuery={false}
+                isPublic={viewNote.isPublic}
+                triggerUrl={viewNote.triggerUrl}
+              />
+            </div>
+          )}
+          {view === 'noteComments' && (
+            <NoteComments
+              note={props.note}
+              comments={props.comments}
+              triggerUrl={props.triggerUrl}
+            />
+          )}
+          {view === 'viewNoteLikes' && (
+            <Paper padding="1.5rem">
+              <ViewLikesModal users={props.likedBy} />
+            </Paper>
+          )}
+        </Box>
+      </Container>
+    </MuiModal>
+  )
 }
 
 Modal.propTypes = {
-	openModal: PropTypes.bool,
-	handleClose: PropTypes.func,
-	disableBackdropClick: PropTypes.bool,
-	view: PropTypes.string,
-	embedUrl: PropTypes.string,
-	user: PropTypes.object,
-	viewNote: PropTypes.object,
-	prevNote: PropTypes.object,
-	prevJournal: PropTypes.object,
-	note: PropTypes.object,
-	comments: PropTypes.array,
+  openModal: PropTypes.bool,
+  handleClose: PropTypes.func,
+  disableBackdropClick: PropTypes.bool,
+  view: PropTypes.string,
+  embedUrl: PropTypes.string,
+  user: PropTypes.object,
+  viewNote: PropTypes.object,
+  prevNote: PropTypes.object,
+  prevJournal: PropTypes.object,
+  note: PropTypes.object,
+  comments: PropTypes.array,
+  likedBy: PropTypes.array,
 }
 
 export default Modal
---
+++ src/components/ViewLikes/index.js
@@ -0,0 +1,23 @@
+import { Avatar, Typography } from '@material-ui/core'
+import { AvatarGroup } from '@material-ui/lab'
+import React from 'react'
+import { useStyles } from './style'
+
+export default function ViewLikes(props) {
+  const classes = useStyles()
+  const { users, onClick } = props
+  return (
+    <div className={classes.root} onClick={onClick}>
+      <AvatarGroup className={classes.avatarGroup} max={3}>
+        {users.length > 0 &&
+          users.map(user => (
+            <Avatar alt="user image" src={user.profileImage?.url} />
+          ))}
+      </AvatarGroup>
+      <Typography variant="body2">
+        Liked by <b>{users[users.length - 1]?.username}</b>
+        {`${users.length > 1 ? ` and ${users.length - 1} others` : ''}`}
+      </Typography>
+    </div>
+  )
+}
