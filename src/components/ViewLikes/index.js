3b7b9b06a5dd42164d798c3b7c76c29f29954c8c
Fisayo Watti <wf.watti@gmail.com>
2021-02-15 15:54:34 +0100

    Add functionality to quantify amount units

--- src/components/ViewLikes/index.js
+++ src/components/ViewLikes/index.js
@@ -1,11 +1,13 @@
 import { Avatar, Typography } from '@material-ui/core'
 import { AvatarGroup } from '@material-ui/lab'
 import React from 'react'
+import { quantify } from '../../helpers'
 import { useStyles } from './style'
 
 export default function ViewLikes(props) {
   const classes = useStyles()
   const { users, onClick } = props
+
   return (
     <div className={classes.root} onClick={onClick}>
       <AvatarGroup className={classes.avatarGroup} max={3}>
@@ -16,7 +18,11 @@ export default function ViewLikes(props) {
       </AvatarGroup>
       <Typography variant="body2">
         Liked by <b>{users[users.length - 1]?.username}</b>
-        {`${users.length > 1 ? ` and ${users.length - 1} others` : ''}`}
+        {`${
+          users.length > 1
+            ? ` and ${users.length - 1} ${quantify(users.length - 1, 'other')}`
+            : ''
+        }`}
       </Typography>
     </div>
   )
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,61 +1,104 @@
-export const getProfileCompletion = (user) => {
-    const volunteerProfileKeys = [
-        'profileImage' ,'firstName', 'lastName', 'username', 'phoneNumber', 'DateOfBirth',
-        'gender', 'maritalStatus', 'personality_type', 'occupation',
-        'experience', 'availableDays', 'availableTime', 'charity', 'topics'
-    ]
+export const getProfileCompletion = user => {
+  const volunteerProfileKeys = [
+    'profileImage',
+    'firstName',
+    'lastName',
+    'username',
+    'phoneNumber',
+    'DateOfBirth',
+    'gender',
+    'maritalStatus',
+    'personality_type',
+    'occupation',
+    'experience',
+    'availableDays',
+    'availableTime',
+    'charity',
+    'topics',
+  ]
 
-    const guestProfileKeys = [
-        'profileImage' ,'firstName', 'lastName', 'username', 'phoneNumber', 'DateOfBirth',
-        'gender', 'maritalStatus', 'personality_type', 'occupation',
-        'experience', 'availableDays', 'availableTime', 'topics'
-    ]
+  const guestProfileKeys = [
+    'profileImage',
+    'firstName',
+    'lastName',
+    'username',
+    'phoneNumber',
+    'DateOfBirth',
+    'gender',
+    'maritalStatus',
+    'personality_type',
+    'occupation',
+    'experience',
+    'availableDays',
+    'availableTime',
+    'topics',
+  ]
 
-    const profileKeys = user.userType === 'Volunteer' ? volunteerProfileKeys : guestProfileKeys;
+  const profileKeys =
+    user.userType === 'Volunteer' ? volunteerProfileKeys : guestProfileKeys
 
-    //handle empty array for calculation.
+  //handle empty array for calculation.
 
-    const profileScore = profileKeys.reduce((acc, curr) => {
-        if (user){
-            if(Array.isArray(user[curr]) && user[curr].length > 0){
-                acc += 1
-            } else if (!Array.isArray(user[curr]) && user[curr]) {
-                acc += 1
-            }
-        }
-        return acc
-    }, 0)
+  const profileScore = profileKeys.reduce((acc, curr) => {
+    if (user) {
+      if (Array.isArray(user[curr]) && user[curr].length > 0) {
+        acc += 1
+      } else if (!Array.isArray(user[curr]) && user[curr]) {
+        acc += 1
+      }
+    }
+    return acc
+  }, 0)
 
-    const profilePercentage = `${Math.round((profileScore / profileKeys.length) * 100)}%`
+  const profilePercentage = `${Math.round(
+    (profileScore / profileKeys.length) * 100
+  )}%`
 
-    return profilePercentage
+  return profilePercentage
 }
 
 export const getGroupName = (mode, usersDetails, user) => {
+  const members = usersDetails.filter(det => det.isPresent) //if a user isPresent is false then don't render it
 
-    const members = usersDetails.filter(det => det.isPresent) //if a user isPresent is false then don't render it
+  const membersMinusUser = members
+    .filter((member, memberIndex) => member.userId !== user?.id)
+    .map(item => item.username)
+  const displayLimit = mode == 'chatList' ? 2 : 4
 
-    const membersMinusUser = members.filter((member, memberIndex) => member.userId !== user?.id).map(item => item.username); 
-    const displayLimit = mode == 'chatList' ? 2 : 4
+  const more =
+    membersMinusUser.length - displayLimit > 0
+      ? `+${membersMinusUser.length - displayLimit}`
+      : ''
 
-    const more = (membersMinusUser.length - displayLimit > 0) ? `+${membersMinusUser.length - displayLimit}` : '';
+  const truncateValue = more ? 11 : 14
 
-    const truncateValue = more ? 11 : 14
+  const name =
+    mode == 'chatList'
+      ? membersMinusUser
+          .filter((member, memberIndex) => memberIndex < displayLimit)
+          .join(', ')
+          .substring(0, truncateValue)
+      : membersMinusUser
+          .filter((member, memberIndex) => memberIndex < displayLimit)
+          .join(', ')
 
-    const name = mode == 'chatList'
-                    ?membersMinusUser.filter((member, memberIndex) => memberIndex < displayLimit).join(', ').substring(0, truncateValue)
-                    :membersMinusUser.filter((member, memberIndex) => memberIndex < displayLimit).join(', ')
+  const finalName = more && mode == 'chatList' ? name + '...' : name
 
-    const finalName = more && mode == 'chatList' ? name + '...' : name
+  // console.log('e b tings', {usersDetails, members, membersMinusUser, more, truncateValue, name, finalName} )
 
-    // console.log('e b tings', {usersDetails, members, membersMinusUser, more, truncateValue, name, finalName} )
-
-    return {
-        name: finalName,
-        more
-    }
+  return {
+    name: finalName,
+    more,
+  }
 }
 
+export const quantify = (num, str) => {
+  if (num > 1) {
+    return str + 's'
+  } else {
+    return str
+  }
+}
 
 // export const checkIfGroupNameChanged = (initialName, groupName) => {
 //     console.log('INITIAL NAME', initialName, 'GROUP NAME', groupName)
